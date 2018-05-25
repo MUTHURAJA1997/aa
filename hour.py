96dde3924587d5f53d3cf0da73903d7bc423be16
MUTHURAJA1997 <37202587+MUTHURAJA1997@users.noreply.github.com>
2018-05-25 10:12:47 +0530

    Create hour.py

---
+++ hour.py
@@ -0,0 +1,12 @@
+t=int(input("enter the hr 1"))
+x=int(input("enter the min 1"))
+h=int(input("enter the hr 2"))
+f=int(input("enter the min 2"))
+q=(t-h)
+l=x-f
+if(q>0 and l>0):
+ print(q,l)
+else:
+    print((q,-l))
+  
+  
